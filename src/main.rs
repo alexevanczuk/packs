06427c479e9479ebf5ab712393dba88797dd1cdf
Alex Evanczuk <alex.evanczuk@gusto.com>
2023-05-18 22:51:50 -0400

    more simplifications + CI

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,7 @@
-use clap::{Parser, Subcommand};
-use packs::Packs;
-mod packs;
-
-#[derive(Subcommand, Debug)]
-enum Command {
-    Greet,
-}
-
-/// A CLI to interact with packs
-#[derive(Parser, Debug)]
-#[command(author, version, about, long_about = None)]
-struct Args {
-    #[command(subcommand)]
-    command: Command,
-}
+pub mod cli;
+pub mod packs;
+use cli::cli;
 
 fn main() {
     cli()
-}
-
-fn cli() {
-    let args = Args::parse();
-    match args.command {
-        Command::Greet => {
-            Packs::greet();
-        }
-    }
 }
--- src/packs.rs
+++ src/packs.rs
@@ -1,5 +1,4 @@
 pub struct Packs {}
-
 impl Packs {
     pub fn greet() -> () {
         println!("Hello! This CLI is under construction.")
