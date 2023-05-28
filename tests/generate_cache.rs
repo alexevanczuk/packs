02ea540b19ac76523da6c4f85270c6fbec5ec2f5
Alex Evanczuk <alex.evanczuk@gusto.com>
2023-05-28 08:09:56 -0400

    set cody directive

--- tests/generate_cache.rs
+++ tests/generate_cache.rs
@@ -1,6 +1,6 @@
 use assert_cmd::prelude::*;
 use predicates::prelude::*;
-use std::{error::Error, process::Command, path::PathBuf};
+use std::{error::Error, path::PathBuf, process::Command};
 
 #[test]
 fn test_generate_cache() -> Result<(), Box<dyn Error>> {
@@ -18,11 +18,13 @@ fn test_generate_cache() -> Result<(), Box<dyn Error>> {
         .stdout(predicate::str::contains(
             "The file content digests are {\"packs/bar/app/services/bar.rb\": \"f2af2fc657b71331ff3a8c39b48365eb\", \"packs/foo/app/services/foo.rb\": \"4be8effd7ac57323adcb53d0cf0ce789\"}",
         ));
-    Ok(())
-    
+
     let filename = "tests/fixtures/simple_app/packs/foo/app/services/foo.rb";
-    let digest = md5::compute(&filename);
-    // Make this code compile
-    let digest_str: String = digest;
-    let cache_file = PathBuf::from("tests/fixtures/simple_app/tmp/cache/packwerk/").join(digest_str);
+    let digest = md5::compute(filename);
+    let digest_str: String = format!("{:x}", digest);
+    let cache_file = PathBuf::from("tests/fixtures/simple_app/tmp/cache/packwerk/").join(&digest_str);
+    // fs::File::open(cache_file)
+    // Pretty print this json as a rust string and set an expectation that the contents of cache_file are equivalent to this JSON
+    expected_json = { "filename": "tests/fixtures/simple_app/packs/foo/app/services/foo.rb" , "digest": "4be8effd7ac57323adcb53d0cf0ce789", "digest_str": "4be8effd7ac57323adcb53d0cf0ce789", "cache_file": "tests/fixtures/simple_app/tmp/cache/packwerk/4be8effd7ac57323adcb53d0cf0ce789" }
+    Ok(())
 }
