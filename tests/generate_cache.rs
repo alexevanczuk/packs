bee519c02fae550fc81cbe4123e303ce2ab0bac7
Alex Evanczuk <alex.evanczuk@gusto.com>
2023-05-28 08:25:58 -0400

    fix compiler errors

--- tests/generate_cache.rs
+++ tests/generate_cache.rs
@@ -36,8 +36,8 @@ fn test_generate_cache() -> Result<(), Box<dyn Error>> {
 
     let digest = md5::compute("packs/foo/app/services/foo.rb");
     let digest_str = format!("{:x}", digest);
-    let cache_file = PathBuf::from("tests/fixtures/simple_app/tmp/cache/packwerk/").join(&digest_str);
-    let mut file = std::fs::File::open(cache_file).unwrap_or_else(|_| panic!("Failed to open file {:?}", cache_file));
+    let cache_file = PathBuf::from("tests/fixtures/simple_app/tmp/cache/packwerk/").join(digest_str);
+    let mut file = std::fs::File::open(&cache_file).unwrap_or_else(|_| panic!("Failed to open file {:?}", cache_file));
     let mut file_content = Vec::new();
     file.read_to_end(&mut file_content).expect("Failed to read file");
     let file_content_str = String::from_utf8_lossy(&file_content);
