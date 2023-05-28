a1ddce90e0d4ca045875addaa3c2992b996f0c87
Alex Evanczuk <alex.evanczuk@gusto.com>
2023-05-28 08:25:12 -0400

    write failing file cache test

--- tests/generate_cache.rs
+++ tests/generate_cache.rs
@@ -1,6 +1,6 @@
 use assert_cmd::prelude::*;
 use predicates::prelude::*;
-use std::{error::Error, path::PathBuf, process::Command};
+use std::{error::Error, io::Read, path::PathBuf, process::Command};
 
 #[test]
 fn test_generate_cache() -> Result<(), Box<dyn Error>> {
@@ -19,12 +19,28 @@ fn test_generate_cache() -> Result<(), Box<dyn Error>> {
             "The file content digests are {\"packs/bar/app/services/bar.rb\": \"f2af2fc657b71331ff3a8c39b48365eb\", \"packs/foo/app/services/foo.rb\": \"4be8effd7ac57323adcb53d0cf0ce789\"}",
         ));
 
-    let filename = "tests/fixtures/simple_app/packs/foo/app/services/foo.rb";
-    let digest = md5::compute(filename);
-    let digest_str: String = format!("{:x}", digest);
+    let expected_cache_json = r#"{
+            "file_contents_digest": "4be8effd7ac57323adcb53d0cf0ce789",
+            "unresolved_references": [
+                {
+                    "constant_name": "::Bar",
+                    "namespace_path": ["Foo"],
+                    "relative_path": "packs/foo/app/services/foo.rb",
+                    "source_location": {
+                        "line": 3,
+                        "column": 6
+                    }
+                }
+            ]
+        }"#;
+
+    let digest = md5::compute("packs/foo/app/services/foo.rb");
+    let digest_str = format!("{:x}", digest);
     let cache_file = PathBuf::from("tests/fixtures/simple_app/tmp/cache/packwerk/").join(&digest_str);
-    // fs::File::open(cache_file)
-    // Pretty print this json as a rust string and set an expectation that the contents of cache_file are equivalent to this JSON
-    expected_json = { "filename": "tests/fixtures/simple_app/packs/foo/app/services/foo.rb" , "digest": "4be8effd7ac57323adcb53d0cf0ce789", "digest_str": "4be8effd7ac57323adcb53d0cf0ce789", "cache_file": "tests/fixtures/simple_app/tmp/cache/packwerk/4be8effd7ac57323adcb53d0cf0ce789" }
+    let mut file = std::fs::File::open(cache_file).unwrap_or_else(|_| panic!("Failed to open file {:?}", cache_file));
+    let mut file_content = Vec::new();
+    file.read_to_end(&mut file_content).expect("Failed to read file");
+    let file_content_str = String::from_utf8_lossy(&file_content);
+    assert_eq!(file_content_str, expected_cache_json);
     Ok(())
 }
