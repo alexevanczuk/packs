1f301348a1908a4a4ba04bed238386724a11bcb2
Alex Evanczuk <alex.evanczuk@gusto.com>
2023-06-03 18:23:43 -0400

    write failing test

--- src/packs/configuration.rs
+++ src/packs/configuration.rs
@@ -23,13 +23,24 @@ pub(crate) fn get(absolute_root: PathBuf) -> Configuration {
 
 #[cfg(test)]
 mod tests {
+    use glob::Paths;
+
     use super::*;
     use crate::packs::configuration;
+
+    fn unroll_include(include: Paths) -> Vec<PathBuf> {
+        include
+            .map(|p| {
+                p.unwrap_or_else(|err| panic!("Could not read file: {:?}", err))
+            })
+            .collect()
+    }
 
     #[test]
     fn default_options() {
         let absolute_root = PathBuf::from("tests/fixtures/simple_app");
         let actual = configuration::get(absolute_root.clone());
-        assert_eq!(actual.absolute_root, absolute_root)
+        assert_eq!(actual.absolute_root, absolute_root);
+        assert_eq!(unroll_include(actual.include), vec![PathBuf::from("blah")])
     }
 }
