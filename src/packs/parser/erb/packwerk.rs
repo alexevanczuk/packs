c367526b667997709465ed0f93d3dcb45248076a
Alex Evanczuk <alex.evanczuk@gusto.com>
2023-06-06 09:25:23 -0400

    write another failing test

--- src/packs/parser/erb/packwerk.rs
+++ src/packs/parser/erb/packwerk.rs
@@ -38,4 +38,67 @@ mod tests {
             extract_from_contents(contents)
         );
     }
+
+    #[test]
+    fn complex_erb() {
+        let contents: String = String::from(
+            "/
+<!DOCTYPE html>
+<html>
+<head>
+  <title>ERB Snippet</title>
+</head>
+<body>
+  <% if Foo %>
+    <h1>Hello, World!</h1>
+  <% else %>
+    <p>Welcome to the ERB snippet!</p>
+  <% end %>
+
+  <% unless Bar.empty? %>
+    <ul>
+      <% Baz.each do |item| %>
+        <li><%= item %></li>
+      <% end %>
+    </ul>
+  <% end %>
+
+  <% for i in Boo %>
+    <p>Iteration <%= Bee %></p>
+  <% end %>
+</body>
+</html>
+        ",
+        );
+        assert_eq!(
+            vec![
+                UnresolvedReference {
+                    name: String::from("Foo"),
+                    namespace_path: vec![],
+                    location: Range::default()
+                },
+                UnresolvedReference {
+                    name: String::from("Bar"),
+                    namespace_path: vec![],
+                    location: Range::default()
+                },
+                UnresolvedReference {
+                    name: String::from("Baz"),
+                    namespace_path: vec![],
+                    location: Range::default()
+                },
+                UnresolvedReference {
+                    name: String::from("Boo"),
+                    namespace_path: vec![],
+                    location: Range::default()
+                },
+                UnresolvedReference {
+                    name: String::from("Bee"),
+                    namespace_path: vec![],
+                    location: Range::default()
+                }
+            ],
+            extract_from_contents(contents)
+        );
+    }
 }
