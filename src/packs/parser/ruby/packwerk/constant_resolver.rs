beddb2d4cb7937cbc3bd57d6761e45534120fd12
Alex Evanczuk <alex.evanczuk@gusto.com>
2023-06-15 09:32:09 -0400

    switch constant resolver to store a map of constant names to constant structs, making it easier to parallelize building the map

--- src/packs/parser/ruby/packwerk/constant_resolver.rs
+++ src/packs/parser/ruby/packwerk/constant_resolver.rs
@@ -11,7 +11,7 @@ use std::{
 #[allow(dead_code)]
 #[derive(Default)]
 pub struct ConstantResolver {
-    fully_qualified_constant_to_absolute_path_map: HashMap<String, PathBuf>,
+    fully_qualified_constant_to_constant_map: HashMap<String, Constant>,
     // Just for testing
     pub(crate) autoload_paths: Vec<PathBuf>,
 }
@@ -36,9 +36,9 @@ impl ConstantResolver {
         // 6) Join the vector with ::
         // 7) Strip the leading "::" from the string
         // 8) Add the fully qualified constant name to the map, with the value being the absolute path of the file
-        let mut fully_qualified_constant_to_absolute_path_map: HashMap<
+        let mut fully_qualified_constant_to_constant_map: HashMap<
             String,
-            PathBuf,
+            Constant,
         > = HashMap::new();
 
         debug!("Building constant resolver");
@@ -69,14 +69,18 @@ impl ConstantResolver {
                 let fully_qualified_constant_name =
                     format!("::{}", fully_qualified_constant_name);
 
-                fully_qualified_constant_to_absolute_path_map
-                    .insert(fully_qualified_constant_name, file);
+                let constant = Constant {
+                    fully_qualified_name: fully_qualified_constant_name.clone(),
+                    absolute_path_of_definition: file.clone(),
+                };
+                fully_qualified_constant_to_constant_map
+                    .insert(fully_qualified_constant_name, constant);
             }
         }
 
         debug!("Finished building constant resolver");
         ConstantResolver {
-            fully_qualified_constant_to_absolute_path_map,
+            fully_qualified_constant_to_constant_map,
             autoload_paths,
         }
     }
@@ -85,7 +89,7 @@ impl ConstantResolver {
         &self,
         fully_or_partially_qualified_constant: &String,
         namespace_path: &[String],
-    ) -> Option<Constant> {
+    ) -> Option<&Constant> {
         // Example for namespace_path: ['Foo', 'Bar', 'Baz']
         // If the fully_or_partially_qualified_constant is 'Boo',
         // it could refer to any of the following:
@@ -99,9 +103,9 @@ impl ConstantResolver {
         // If the fully_or_partially_qualified_constant is prefixed with ::, we should skip checking the namespace_path
         // because it's an absolute reference.
         if fully_or_partially_qualified_constant.starts_with("::") {
-            let absolute_path = self
-                .fully_qualified_constant_to_absolute_path_map
-                .get(fully_or_partially_qualified_constant);
+            // let absolute_path = self
+            //     .fully_qualified_constant_to_absolute_path_map
+            //     .get(fully_or_partially_qualified_constant);
             if let Some(constant) = self.constant_for_fully_qualified_name(
                 fully_or_partially_qualified_constant,
             ) {
@@ -143,16 +147,12 @@ impl ConstantResolver {
     fn constant_for_fully_qualified_name(
         &self,
         fully_qualified_name: &String,
-    ) -> Option<Constant> {
-        if let Some(absolute_path_of_definition) = self
-            .fully_qualified_constant_to_absolute_path_map
+    ) -> Option<&Constant> {
+        if let Some(constant) = self
+            .fully_qualified_constant_to_constant_map
             .get(fully_qualified_name)
         {
-            return Some(Constant {
-                fully_qualified_name: fully_qualified_name.clone(),
-                absolute_path_of_definition: absolute_path_of_definition
-                    .clone(),
-            });
+            return Some(constant);
         }
 
         None
@@ -179,23 +179,29 @@ mod tests {
 
         let resolver = ConstantResolver::create(&absolute_root, paths);
 
-        let mut expected_file_map: HashMap<String, PathBuf> = HashMap::new();
+        let mut expected_file_map: HashMap<String, Constant> = HashMap::new();
         expected_file_map.insert(
             "::Foo".to_string(),
-            PathBuf::from(
-                "tests/fixtures/simple_app/packs/foo/app/services/foo.rb",
-            ),
+            Constant {
+                fully_qualified_name: "::Foo".to_string(),
+                absolute_path_of_definition: PathBuf::from(
+                    "tests/fixtures/simple_app/packs/foo/app/services/foo.rb",
+                ),
+            },
         );
 
         expected_file_map.insert(
             "::Foo::Bar".to_string(),
-            PathBuf::from(
-                "tests/fixtures/simple_app/packs/foo/app/services/foo/bar.rb",
-            ),
+            Constant {
+                fully_qualified_name: "::Foo::Bar".to_string(),
+                absolute_path_of_definition: PathBuf::from(
+                    "tests/fixtures/simple_app/packs/foo/app/services/foo/bar.rb",
+                ),
+            }
         );
 
         let actual_file_map =
-            &resolver.fully_qualified_constant_to_absolute_path_map;
+            &resolver.fully_qualified_constant_to_constant_map;
 
         assert_eq!(&expected_file_map, actual_file_map);
     }
@@ -208,7 +214,7 @@ mod tests {
         let resolver = configuration::get(&absolute_root).constant_resolver;
 
         assert_eq!(
-            Constant {
+            &Constant {
                 fully_qualified_name: "::Foo".to_string(),
                 absolute_path_of_definition: absolute_root
                     .join("packs/foo/app/services/foo.rb")
@@ -223,7 +229,7 @@ mod tests {
             .unwrap();
         let resolver = configuration::get(&absolute_root).constant_resolver;
         assert_eq!(
-            Constant {
+            &Constant {
                 fully_qualified_name: "::Foo".to_string(),
                 absolute_path_of_definition: absolute_root
                     .join("packs/foo/app/services/foo.rb")
@@ -248,7 +254,7 @@ mod tests {
             .unwrap();
         let resolver = configuration::get(&absolute_root).constant_resolver;
         assert_eq!(
-            Constant {
+            &Constant {
                 fully_qualified_name: "::Foo::Bar".to_string(),
                 absolute_path_of_definition: absolute_root
                     .join("packs/foo/app/services/foo/bar.rb")
@@ -266,7 +272,7 @@ mod tests {
             .unwrap();
         let resolver = configuration::get(&absolute_root).constant_resolver;
         assert_eq!(
-            Constant {
+            &Constant {
                 fully_qualified_name: "::Bar".to_string(),
                 absolute_path_of_definition: absolute_root
                     .join("packs/bar/app/services/bar.rb")
