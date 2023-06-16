a50d2285e4dcec12ec72a0381710862ef9c38705
Alex Evanczuk <alex.evanczuk@gusto.com>
2023-06-16 02:19:51 -0400

    serialize package_todo file better

--- src/packs/package_todo.rs
+++ src/packs/package_todo.rs
@@ -1,5 +1,6 @@
-use serde::{Deserialize, Serialize};
-use std::collections::HashMap;
+use serde::{ser::SerializeMap, Deserialize, Serialize, Serializer};
+use std::collections::{HashMap, HashSet};
+use tracing::debug;
 
 use super::{Configuration, Violation};
 
@@ -7,13 +8,49 @@ use super::{Configuration, Violation};
 pub struct ViolationGroup {
     // Use serde rename to parse the key as violations
     #[serde(rename = "violations")]
-    pub violation_types: Vec<String>,
-    pub files: Vec<String>,
+    pub violation_types: HashSet<String>,
+    #[serde(serialize_with = "serialize_sorted_set")]
+    pub files: HashSet<String>,
 }
 
+fn serialize_sorted_set<S>(
+    set: &HashSet<String>,
+    serializer: S,
+) -> Result<S::Ok, S::Error>
+where
+    S: Serializer,
+{
+    let mut sorted_files: Vec<&String> = set.iter().collect();
+    sorted_files.sort();
+    sorted_files.serialize(serializer)
+}
+
+fn serialize_violations_by_defining_pack<S>(
+    map: &HashMap<String, HashMap<String, ViolationGroup>>,
+    serializer: S,
+) -> Result<S::Ok, S::Error>
+where
+    S: Serializer,
+{
+    let mut sorted_entries: Vec<_> = map.iter().collect();
+    sorted_entries.sort_by_key(|(k, _)| k.as_str());
+
+    let mut map_serializer =
+        serializer.serialize_map(Some(sorted_entries.len()))?;
+
+    for (key, value) in sorted_entries {
+        let quoted_value: HashMap<_, _> = value
+            .iter()
+            .map(|(k, v)| (format!("QUOTE{}QUOTE", k), v))
+            .collect();
+        map_serializer.serialize_entry(key, &quoted_value)?;
+    }
+
+    map_serializer.end()
+}
 #[derive(PartialEq, Eq, Debug, Deserialize, Serialize, Default, Clone)]
 pub struct PackageTodo {
-    #[serde(flatten)]
+    #[serde(flatten, serialize_with = "serialize_violations_by_defining_pack")]
     pub violations_by_defining_pack:
         HashMap<String, HashMap<String, ViolationGroup>>,
 }
@@ -22,6 +59,7 @@ pub fn write_violations_to_disk(
     configuration: Configuration,
     violations: Vec<Violation>,
 ) {
+    debug!("Starting writing violations to disk");
     // First we need to group the violations by the repsonsible pack, which today is always the referencing pack
     // Later if we change where a violation shows up, we should delegate to the checker
     // to decide what pack it should be in.
@@ -37,11 +75,33 @@ pub fn write_violations_to_disk(
     }
 
     // Then we group violations by the defining pack, since that's how they're grouped in the package_todo.yml file
-    for (responsible_pack_name, violations) in violations_by_responsible_pack {
+    for (responsible_pack_name, mut violations) in
+        violations_by_responsible_pack
+    {
         let mut violations_by_defining_pack: HashMap<
             String,
             HashMap<String, ViolationGroup>,
         > = HashMap::new();
+        // Sort violations by the defining pack name, then constant name, then file name
+        // This ensures they show up deterministically in the package_todo.yml file.
+        violations.sort_by(|a, b| {
+            let defining_pack_name_comparison = a
+                .identifier
+                .defining_pack_name
+                .cmp(&b.identifier.defining_pack_name);
+            if defining_pack_name_comparison != std::cmp::Ordering::Equal {
+                return defining_pack_name_comparison;
+            }
+
+            let constant_name_comparison =
+                a.identifier.constant_name.cmp(&b.identifier.constant_name);
+            if constant_name_comparison != std::cmp::Ordering::Equal {
+                return constant_name_comparison;
+            }
+
+            a.identifier.file.cmp(&b.identifier.file)
+        });
+
         for violation in violations {
             let defining_pack_name =
                 violation.identifier.defining_pack_name.to_owned();
@@ -56,10 +116,10 @@ pub fn write_violations_to_disk(
 
             violation_group
                 .files
-                .push(violation.identifier.file.to_owned());
+                .insert(violation.identifier.file.to_owned());
             violation_group
                 .violation_types
-                .push(violation.identifier.violation_type.to_owned());
+                .insert(violation.identifier.violation_type.to_owned());
         }
 
         let package_todo = PackageTodo {
@@ -75,14 +135,31 @@ pub fn write_violations_to_disk(
             .join("package_todo.yml");
 
         let package_todo_yml = serde_yaml::to_string(&package_todo).unwrap();
+        let header = format!("
+            # This file contains a list of dependencies that are not part of the long term plan for the
+            # '{}' package.
+            # We should generally work to reduce this list over time.
+            #
+            # You can regenerate this file using the following command:
+            #
+            # bin/packwerk update-todo
+            ---
+        ", responsible_pack_name);
+        let package_todo_yml = header + &package_todo_yml;
 
         if !package_todo_yml_absolute_filepath.exists() {
             std::fs::File::create(&package_todo_yml_absolute_filepath).unwrap();
         }
 
+        // Replace all instances of QUOTE with ".
+        // This is a hack to work around the fact that I can't figure out how to make serde_yaml
+        // double quote some strings but not others.
+        let package_todo_yml = package_todo_yml.replace("QUOTE", "\"");
         std::fs::write(package_todo_yml_absolute_filepath, package_todo_yml)
             .unwrap();
     }
+
+    debug!("Finished writing violations to disk");
 }
 
 #[cfg(test)]
@@ -121,21 +198,28 @@ mod tests {
             HashMap<String, ViolationGroup>,
         > = HashMap::new();
         let mut bar_violations = HashMap::new();
+        let mut violation_types = HashSet::new();
+        violation_types.insert(String::from("dependency"));
+        let mut files = HashSet::new();
+        files.insert(String::from("packs/foo/app/services/foo.rb"));
         bar_violations.insert(
             String::from("::Bar"),
             ViolationGroup {
-                violation_types: vec![String::from("dependency")],
-                files: vec![String::from("packs/foo/app/services/foo.rb")],
+                violation_types,
+                files,
             },
         );
+
+        let mut violation_types = HashSet::new();
+        violation_types.insert(String::from("dependency"));
+        violation_types.insert(String::from("privacy"));
+        let mut files = HashSet::new();
+        files.insert(String::from("packs/foo/app/services/foo.rb"));
         bar_violations.insert(
             String::from("::Baz"),
             ViolationGroup {
-                violation_types: vec![
-                    String::from("dependency"),
-                    String::from("privacy"),
-                ],
-                files: vec![String::from("packs/foo/app/services/foo.rb")],
+                violation_types,
+                files,
             },
         );
         violations_by_defining_pack
