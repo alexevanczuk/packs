dd003fbd1e3fce2bf3e42821378b18c10a13a427
Alex Evanczuk <alex.evanczuk@gusto.com>
2023-06-26 18:09:53 -0400

    extract acronyms into its own function

--- src/packs/parser/ruby/packwerk/constant_resolver.rs
+++ src/packs/parser/ruby/packwerk/constant_resolver.rs
@@ -40,6 +40,33 @@ fn inferred_constant_from_file(
         absolute_path_of_definition: absolute_path.to_path_buf(),
     }
 }
+
+// Load in config/initializers/inflections.rb
+// For any inflections in there, add them to the acronyms vector
+// An inflection takes the form of "inflect.acronym 'API'", so "API" would be the acronym here
+// This is a bit of a hack, but it's the easiest way to get the inflections loaded in
+// TODO: Figure out a better way to do this
+fn get_acronyms_from_disk(absolute_root: &Path) -> HashSet<String> {
+    let mut acronyms: HashSet<String> = HashSet::new();
+
+    let inflections_path =
+        absolute_root.join("config/initializers/inflections.rb");
+    if inflections_path.exists() {
+        let inflections_file =
+            std::fs::read_to_string(inflections_path).unwrap();
+        let inflections_lines = inflections_file.lines();
+        for line in inflections_lines {
+            if line.contains(".acronym") {
+                let re = Regex::new(r#"['\\"]"#).unwrap();
+                let acronym = re.split(line).nth(1).unwrap();
+                acronyms.insert(acronym.to_string());
+            }
+        }
+    }
+
+    acronyms
+}
+
 #[allow(unused_variables)]
 impl ConstantResolver {
     pub fn create(
@@ -62,28 +89,6 @@ impl ConstantResolver {
 
         debug!("Building constant resolver");
 
-        let mut acronyms: HashSet<String> = HashSet::new();
-
-        // Load in config/initializers/inflections.rb
-        // For any inflections in there, add them to the acronyms vector
-        // An inflection takes the form of "inflect.acronym 'API'", so "API" would be the acronym here
-        // This is a bit of a hack, but it's the easiest way to get the inflections loaded in
-        // TODO: Figure out a better way to do this
-        let inflections_path =
-            absolute_root.join("config/initializers/inflections.rb");
-        if inflections_path.exists() {
-            let inflections_file =
-                std::fs::read_to_string(inflections_path).unwrap();
-            let inflections_lines = inflections_file.lines();
-            for line in inflections_lines {
-                if line.contains(".acronym") {
-                    let re = Regex::new(r#"['\\"]"#).unwrap();
-                    let acronym = re.split(line).nth(1).unwrap();
-                    acronyms.insert(acronym.to_string());
-                }
-            }
-        }
-
         let constants: Vec<Constant> = autoload_paths
             .iter()
             .par_bridge()
@@ -100,7 +105,7 @@ impl ConstantResolver {
                         inferred_constant_from_file(
                             &file,
                             absolute_autoload_path,
-                            &acronyms,
+                            &get_acronyms_from_disk(absolute_root),
                         )
                     })
                     .collect::<Vec<Constant>>()
