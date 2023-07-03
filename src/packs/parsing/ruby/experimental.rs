aba49d1500c572ba2017577c9c39fcf4e6fa5a2a
Alex Evanczuk <alex.evanczuk@gusto.com>
2023-07-02 22:05:57 -0400

    add two failing tests

--- src/packs/parsing/ruby/experimental.rs
+++ src/packs/parsing/ruby/experimental.rs
@@ -5,7 +5,7 @@ mod tests {
     use std::path::PathBuf;
 
     use crate::packs::parsing::ruby::experimental::parser::process_from_contents;
-    use crate::packs::parsing::Range;
+    use crate::packs::parsing::{Definition, Range};
     use crate::packs::{ProcessedFile, UnresolvedReference};
 
     #[test]
@@ -106,6 +106,63 @@ mod tests {
         }];
 
         let definitions = vec![];
+
+        let actual = process_from_contents(contents, &absolute_path);
+        let expected = ProcessedFile {
+            absolute_path,
+            unresolved_references,
+            definitions,
+        };
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn class_definition_no_body() {
+        let contents: String = String::from(
+            "\
+class Foo
+end
+            ",
+        );
+
+        let absolute_path = PathBuf::from("path/to/file.rb");
+        let unresolved_references = vec![];
+
+        let definitions = vec![];
+
+        let actual = process_from_contents(contents, &absolute_path);
+        let expected = ProcessedFile {
+            absolute_path,
+            unresolved_references,
+            definitions,
+        };
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn class_definition_some_body() {
+        let contents: String = String::from(
+            "\
+class Foo
+  def foo
+  end
+end
+            ",
+        );
+
+        let absolute_path = PathBuf::from("path/to/file.rb");
+        let unresolved_references = vec![];
+
+        let definitions = vec![Definition {
+            fully_qualified_name: String::from("Foo"),
+            location: Range {
+                start_row: 1,
+                start_col: 0,
+                end_row: 4,
+                end_col: 3,
+            },
+            namespace_path: vec![],
+        }];
 
         let actual = process_from_contents(contents, &absolute_path);
         let expected = ProcessedFile {
