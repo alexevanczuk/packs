3ca3f914dc767a811f8364c438c9ff8fbe83c057
Alex Evanczuk <alex.evanczuk@gusto.com>
2023-07-04 21:29:57 -0400

    Revert "WIP reference_graph_builder"
    This reverts commit 27a44e01c40cbe3f5a3d7395cdf4f020a56a16f7.

--- src/packs/reference_graph_builder.rs
+++
@@ -1,9 +0,0 @@
-use super::{
-    checker::Reference,
-    parsing::ruby::packwerk::constant_resolver::ConstantResolver,
-};
-
-pub struct ReferenceGraph {
-    pub references: Vec<Reference>,
-    pub constant_resolver: ConstantResolver,
-}
