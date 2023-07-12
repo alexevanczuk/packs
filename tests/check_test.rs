ac316a5ba005b122e4132ce89ea150b95f4c07d9
Alex Evanczuk <alexevanczuk@gmail.com>
2023-07-12 12:19:11 -0400

    Fix bug with experimental parser and single file inputs (#87)
    * add failing test
    
    * refactor existing get_all_references function
    
    * fix test
    
    * bump version

--- tests/check_test.rs
+++ tests/check_test.rs
@@ -22,6 +22,44 @@ fn test_check() -> Result<(), Box<dyn Error>> {
 }
 
 #[test]
+fn test_check_with_single_file() -> Result<(), Box<dyn Error>> {
+    Command::cargo_bin("packs")?
+        .arg("--project-root")
+        .arg("tests/fixtures/simple_app")
+        .arg("--debug")
+        .arg("check")
+        .arg("packs/foo/app/services/foo.rb")
+        .assert()
+        .failure()
+        .stdout(predicate::str::contains("2 violation(s) detected:"))
+        .stdout(predicate::str::contains("packs/foo/app/services/foo.rb:3:4\nDependency violation: `::Bar` belongs to `packs/bar`, but `packs/foo/package.yml` does not specify a dependency on `packs/bar`."))
+        .stdout(predicate::str::contains("packs/foo/app/services/foo.rb:3:4\nPrivacy violation: `::Bar` is private to `packs/bar`, but referenced from `packs/foo`"));
+
+    common::teardown();
+    Ok(())
+}
+
+#[test]
+fn test_check_with_single_file_experimental_parser(
+) -> Result<(), Box<dyn Error>> {
+    Command::cargo_bin("packs")?
+        .arg("--project-root")
+        .arg("tests/fixtures/simple_app")
+        .arg("--debug")
+        .arg("--experimental-parser")
+        .arg("check")
+        .arg("packs/foo/app/services/foo.rb")
+        .assert()
+        .failure()
+        .stdout(predicate::str::contains("2 violation(s) detected:"))
+        .stdout(predicate::str::contains("packs/foo/app/services/foo.rb:3:4\nDependency violation: `::Bar` belongs to `packs/bar`, but `packs/foo/package.yml` does not specify a dependency on `packs/bar`."))
+        .stdout(predicate::str::contains("packs/foo/app/services/foo.rb:3:4\nPrivacy violation: `::Bar` is private to `packs/bar`, but referenced from `packs/foo`"));
+
+    common::teardown();
+    Ok(())
+}
+
+#[test]
 fn test_check_with_package_todo_file() -> Result<(), Box<dyn Error>> {
     Command::cargo_bin("packs")?
         .arg("--project-root")
