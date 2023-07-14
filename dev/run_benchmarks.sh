29cc5890789f2d74b9275f1ba284b3760f52d6fd
Alex Evanczuk <alex.evanczuk@gusto.com>
2023-07-14 11:56:36 -0400

    update benchmarks

--- dev/run_benchmarks.sh
+++ dev/run_benchmarks.sh
@@ -6,7 +6,7 @@ if [ -f "tmp/packs_benchmarks.md" ]; then
 fi
 
 echo "I use https://github.com/sharkdp/hyperfine to benchmark, which makes it easy to get consistent benchmarks. Note that benchmarks are done with cache only. While it's interesting to see the performance improvement on a cold cache, it's not representative of the performance of the tool in a real-world scenario, since most of the time the cache will be warm." >> tmp/packs_benchmarks.md
-echo "To run these benchmarks on your application, you can place this repo next to your rails application and run ../pks/dev/run_benchmarks.sh from the root of your application" >> tmp/packs_benchmarks.md
+echo "To run these benchmarks on your application, you can place this repo next to your rails application and run bash ../pks/dev/run_benchmarks.sh from the root of your application" >> tmp/packs_benchmarks.md
 
 echo -e "\n## Hot Cache, with and without spring, entire codebase" >> tmp/packs_benchmarks.md
 
