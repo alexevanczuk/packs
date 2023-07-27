85eb17cf801ba524fb41b3695997e12df552d8a2
Alex Evanczuk <alex.evanczuk@gusto.com>
2023-07-27 17:11:28 -0400

    add failing test

--- tests/update_test.rs
+++ tests/update_test.rs
@@ -140,3 +140,46 @@ packs/bar:
 
     Ok(())
 }
+
+#[test]
+fn test_update_with_packs_first_app() -> Result<(), Box<dyn Error>> {
+    Command::cargo_bin("packs")?
+        .arg("--project-root")
+        .arg("tests/fixtures/simple_packs_first_app")
+        .arg("update")
+        .assert()
+        .success()
+        .stdout(predicate::str::contains(
+            "Successfully updated package_todo.yml files!",
+        ));
+
+    let package_todo_yml_filepath = Path::new(
+        "tests/fixtures/simple_packs_first_app/packs/foo/package_todo.yml",
+    );
+    let actual = std::fs::read_to_string(package_todo_yml_filepath)?;
+    let expected = String::from(
+        "\
+# This file contains a list of dependencies that are not part of the long term plan for the
+# 'packs/foo' package.
+# We should generally work to reduce this list over time.
+#
+# You can regenerate this file using the following command:
+#
+# pks update
+---
+packs/bar:
+  \"::Bar\":
+    violations:
+    - dependency
+    - privacy
+    files:
+    - packs/foo/app/services/foo.rb
+",
+    );
+    std::fs::remove_file(package_todo_yml_filepath)?;
+    assert_eq!(expected, actual);
+
+    common::teardown();
+
+    Ok(())
+}
