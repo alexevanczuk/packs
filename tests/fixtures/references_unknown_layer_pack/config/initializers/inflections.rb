893bc5438b26c29e8afd51f343e8b2d117076d9c
Perry Hertler <perry.hertler@gusto.com>
2023-12-19 16:54:52 -0600

    adding anyhow to simply result errors

--- tests/fixtures/references_unknown_layer_pack/config/initializers/inflections.rb
+++
@@ -1,6 +0,0 @@
-ActiveSupport::Inflector.inflections do |do_not_couple_implementation_to_this_string|
-  do_not_couple_implementation_to_this_string.acronym 'API'
-
-  # Using single vs double quotes inconsistently
-  do_not_couple_implementation_to_this_string.acronym "CSV"
-end
--- tests/fixtures/references_unknown_layer_pack/packs/feature_flags/app/services/feature_flags.rb
+++
@@ -1,2 +0,0 @@
-# feature_flags, a utility pack, should not rely on Payments, a product pack
-Payments
