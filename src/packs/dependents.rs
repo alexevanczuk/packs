b81cfb196357d21311a162dc3f3559254f0f559c
Perry Hertler <perry.hertler@gusto.com>
2024-03-06 14:39:41 -0600

    finding violation dependents

--- src/packs/dependents.rs
+++ src/packs/dependents.rs
@@ -26,21 +26,48 @@ pub fn find_dependents(
 ) -> anyhow::Result<Dependents> {
     let pack = configuration.pack_set.for_pack(pack_name)?;
 
-    let mut public_dependents: HashSet<PackName> = configuration
+    let public_dependents: HashSet<PackName> = configuration
         .pack_set
         .packs
         .iter()
         .filter(|p| {
             dbg!(&p.name);
             dbg!(&p.dependencies);
-            &p.name != &pack.name && p.dependencies.contains(&pack.name)
+            p.name != pack.name && p.dependencies.contains(&pack.name)
         })
         .map(|p| p.name.clone())
         .collect();
 
+    let mut violation_dependents: HashMap<
+        PackName,
+        HashMap<ViolationType, ViolationCount>,
+    > = HashMap::new();
+
+    for current_pack in &configuration.pack_set.packs {
+        if current_pack.name != pack.name {
+            for (violation_pack_name, violation_groups) in
+                &current_pack.package_todo.violations_by_defining_pack
+            {
+                if violation_pack_name == &pack.name {
+                    for violation_group in violation_groups.values() {
+                        let entry = violation_dependents
+                            .entry(current_pack.name.clone())
+                            .or_default();
+                        for violation_type in &violation_group.violation_types {
+                            entry
+                                .entry(violation_type.clone())
+                                .and_modify(|e| *e += 1)
+                                .or_insert(1);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
     Ok(Dependents {
         public_dependents,
-        ..Default::default()
+        violation_dependents,
     })
 }
 
@@ -64,6 +91,7 @@ mod tests {
         let dependents = find_dependents(&configuration, "packs/baz").unwrap();
         assert_eq!(dependents.public_dependents.len(), 1);
         assert!(dependents.public_dependents.contains("packs/foo"));
+        assert_eq!(dependents.violation_dependents.len(), 0);
     }
 
     #[test]
@@ -79,5 +107,22 @@ mod tests {
         let dependents = find_dependents(&configuration, "packs/bar").unwrap();
         assert_eq!(dependents.public_dependents.len(), 0);
         assert_eq!(dependents.violation_dependents.len(), 1);
+        assert_eq!(
+            dependents
+                .violation_dependents
+                .get("packs/foo")
+                .unwrap()
+                .len(),
+            1
+        );
+        assert_eq!(
+            dependents
+                .violation_dependents
+                .get("packs/foo")
+                .unwrap()
+                .get("dependency")
+                .unwrap(),
+            &1usize
+        );
     }
 }
