c7693b1542ba6a19f38a27e06ff04d9d8eee0b43
Alex Evanczuk <alexevanczuk@gmail.com>
2024-05-25 19:35:30 -0400

    change color to cyan

--- src/packs/checker/output_helper.rs
+++ src/packs/checker/output_helper.rs
@@ -2,7 +2,7 @@ use super::reference::Reference;
 
 pub fn print_reference_location(reference: &Reference) -> String {
     format!(
-        "\x1b[34m{}\x1b[0m:{}:{}\n",
+        "\x1b[36m{}\x1b[0m:{}:{}\n",
         reference.relative_referencing_file,
         reference.source_location.line,
         reference.source_location.column,
